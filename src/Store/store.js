89cb91e0dfefda8684ec320f092259e7f7d65542
moemad <moemad504@gmail.com>
2025-09-14 01:58:06 +0300

    feat: add Courses logic
    Implemented Redux slice and Firestore integration

---
+++ src/Store/store.js
@@ -0,0 +1,9 @@
+// store.js
+import { configureStore } from "@reduxjs/toolkit";
+import { coursesReducer } from "../features/courses/coursesSlice";
+
+export const store = configureStore({
+  reducer: {
+    course: coursesReducer,
+  },
+});
