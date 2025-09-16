cac624d33a9228bd726e7fe26a6c4ada92fba3d2
Alshaimaa <alshaimaaabdelnaeem@gmail.com>
2025-09-16 20:18:58 +0300

    instructor sign up

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/newCourse" element={<NewCourse />} />
+<<<<<<< Updated upstream
         <Route path="/MainDahsbord" element={<MainDashboard />} />
+=======
+        <Route path="/MainDahsbord" element={<CoursesDahsbord />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+         <Route path="/instructorsignup" element={<InstructorSignUp/>} />
+        <Route path="/checkout" element={<Checkout />} />
+>>>>>>> Stashed changes
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
